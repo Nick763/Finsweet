c7eaeb6fedbd07dcf6064b9638198697921971d4
NIKOLAI KUCHUMOV <99647682+Nick763@users.noreply.github.com>
2024-05-04 00:39:43 +0400

    v 2.5
    added some animations

--- js/animations.js
+++ js/animations.js
@@ -1,32 +1,24 @@
 console.log('привет!')
 
 
+// allPosts
 
-let item = document.querySelectorAll(".posts__post-card");
-/* 
-let index = 0;
-setInterval(function() {
-  items[index].classList.add("posts__post-card_selected");
-  for (let i = 0; i < items.length; i++) {
-    if (i !== index) {
-      items[i].classList.contains("posts__post-card_selected") &&
-        items[i].classList.remove("posts__post-card_selected");
-    }
-  }
-  index++;
-  if (index === items.length) {
-    index = 0;
-  }
-}, 2000);
-*/
-
-
-
-document.querySelectorAll('.item').forEach(e=>{
+document.querySelectorAll('.posts__post-card').forEach(e=>{
   e.addEventListener('mouseover',function(){
-    this.classList.add('active_item')
+    this.classList.add('posts__post-card_selected')
   })
   e.addEventListener('mouseout',function(){
-    this.classList.remove('active_item')
+    this.classList.remove('posts__post-card_selected')
+  })
+})
+
+// chooseCatagory
+
+document.querySelectorAll('.choose__item').forEach(e=>{
+  e.addEventListener('mouseover',function(){
+    this.classList.add('choose__item_selected')
+  })
+  e.addEventListener('mouseout',function(){
+    this.classList.remove('choose__item_selected')
   })
 })
